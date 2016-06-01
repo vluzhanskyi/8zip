0e4eb0c818d9ac78acf3153e23d3412cc678f180
vluzhanskyi <Vluzhanskyi@gmail.com>
2016-06-01 13:40:38 +0300

    Try, catch and other crap

--- 8zip/8zip/InputData.cs
+++ 8zip/8zip/InputData.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Ionic.Zip;
-using System.Windows.Forms;
+﻿using Ionic.Zip;
 using System.IO.Compression;
 
 namespace _8zip
